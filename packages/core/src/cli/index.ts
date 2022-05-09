b833f6b1680c1308a461e8157830df6603a30c9f
Chris Krycho <hello@chriskrycho.com>
2022-05-09 07:35:05 -0600

    Add `--build`, `--force`, and `--clean` to CLI
    These options map directly to the `tsc` options, and allow users to
    actually *use* the newly-implemented options.

--- packages/core/src/cli/index.ts
+++ packages/core/src/cli/index.ts
@@ -23,6 +23,23 @@ const { argv } = yargs
     boolean: true,
     description: 'Whether to emit declaration files',
   })
+  .option('build', {
+    alias: 'b',
+    boolean: true,
+    description:
+      'Build one or more projects and their dependencies, if out of date. Same as the TS `--build` flag.',
+    // As with TS itself, we *must* emit declarations when in build mode
+    conflicts: 'declaration',
+  })
+  .option('clean', {
+    implies: 'build',
+    boolean: true,
+    description: 'Delete the outputs of all projects.',
+  })
+  .option('force', {
+    implies: 'build',
+    description: 'Act as if all projects are out of date. Same as the TS `--force` flag.',
+  })
   .option('debug-intermediate-representation', {
     boolean: false,
     description: `When true, writes out a Glint's internal intermediate representation of each file within a GLINT_DEBUG subdirectory of the current working directory. This is intended for debugging Glint itself.`,
